54a2687903c9373297c49d4edbd316348b7d7ac6
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2017-10-10 01:10:20 -0400

    included Class Library  Parsing

---
+++ Parsing/IParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Grammars;
+
+namespace Parsing
+{
+    public interface IParser
+    {
+        bool TryParse(Grammar g, IEnumerable<Token> tokens, out DerivationTree tree);
+    }
+
+    public class ParsingException : Exception
+    {
+    }
+}
