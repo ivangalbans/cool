ea57b07a77abbd7dd4e8a67341822032a74416b5
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2017-10-21 23:38:04 -0400

    writed Terminals of Cool grammar

--- Core/CoolGrammar.cs
+++ Core/CoolGrammar.cs
@@ -40,6 +40,55 @@ namespace Core
             var Expressions     = _coolGrammar.NonTerminal("Expressions");
             #endregion
 
+            #region Terminals
+            var semicolon = _coolGrammar.Terminal(";");
+            var cclass = _coolGrammar.Terminal("class");
+            var TYPE = _coolGrammar.Terminal("TYPE");
+            var openBrace = _coolGrammar.Terminal("{");
+            var closedBrace = _coolGrammar.Terminal("}");
+            var openBracket = _coolGrammar.Terminal("(");
+            var closedBracket = _coolGrammar.Terminal(")");
+            var inherits = _coolGrammar.Terminal("inherits");
+            var epsilon = _coolGrammar.Epsilon;
+            var ID = _coolGrammar.Terminal("ID");
+            var colon = _coolGrammar.Terminal(":");
+            var comma = _coolGrammar.Terminal(",");
+            var assign = _coolGrammar.Terminal("<-");
+            var let = _coolGrammar.Terminal("let");
+            var cin = _coolGrammar.Terminal("in");
+            var not = _coolGrammar.Terminal("not");
+            var less = _coolGrammar.Terminal("<");
+            var lessEqual = _coolGrammar.Terminal("<=");
+            var greater = _coolGrammar.Terminal(">");
+            var greaterEqual = _coolGrammar.Terminal(">=");
+            var equal = _coolGrammar.Terminal("=");
+            var add = _coolGrammar.Terminal("+");
+            var sub = _coolGrammar.Terminal("-");
+            var mul = _coolGrammar.Terminal("*");
+            var div = _coolGrammar.Terminal("/");
+            var neg = _coolGrammar.Terminal("~");
+            var isvoid = _coolGrammar.Terminal("isvoid");
+            var point = _coolGrammar.Terminal(".");
+            var arroba = _coolGrammar.Terminal("@");
+            var cif = _coolGrammar.Terminal("if");
+            var fi = _coolGrammar.Terminal("fi");
+            var then = _coolGrammar.Terminal("then");
+            var celse = _coolGrammar.Terminal("else");
+            var cwhile = _coolGrammar.Terminal("while");
+            var loop = _coolGrammar.Terminal("loop");
+            var pool = _coolGrammar.Terminal("pool");
+            var ccase = _coolGrammar.Terminal("case");
+            var esac = _coolGrammar.Terminal("esac");
+            var of = _coolGrammar.Terminal("of");
+            var cnew = _coolGrammar.Terminal("new");
+            var integer = _coolGrammar.Terminal("integer");
+            var cstring = _coolGrammar.Terminal("string");
+            var ctrue = _coolGrammar.Terminal("true");
+            var cfalse = _coolGrammar.Terminal("false");
+            var cvoid = _coolGrammar.Terminal("void");
+            var lambda = _coolGrammar.Terminal("=>");
+            #endregion
+
         }
     }
 }
