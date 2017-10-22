c25e83a191cd3c5134b1130843cea0e2452c9d69
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2017-10-22 18:10:21 -0400

    created UnitTest project.

---
+++ UnitTest/UnitTestParsing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestParsing
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
