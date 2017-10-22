d4781a735da6825f535a812db296de7c6bcb3ff7
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2017-10-22 01:39:05 -0400

    creating class CoolLexer

---
+++ Core/CoolLexer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grammars;
+using Lexer;
+
+namespace Core
+{
+    class CoolLexer : ILexer<Token>
+    {
+        public IEnumerable<Token> Lex(string str, Grammar g)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
