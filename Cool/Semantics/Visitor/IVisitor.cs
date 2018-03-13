a086bbf8bfa99d224d074c145a0ca073ccdb718c
Ivan Galban Smith <ivan.galban.smith@gmail.com>
2018-03-13 14:22:33 -0400

    implementing Visitor Pattern to semantics checker

---
+++ Cool/Semantics/Visitor/IVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cool.AST;
+
+namespace Cool.Semantics
+{
+    interface IVisitor<TNode> where TNode : ASTNode
+    {
+        void Visit(TNode node, IScope scope, List<SemanticError> errors);
+    }
+}
